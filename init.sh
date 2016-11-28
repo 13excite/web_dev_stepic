bd8eaee68a106f40adf3a6657fc7e0804eb23c91
vladimir <vladimir@MacBook-Pro-vladimir.local>
2016-11-28 03:05:39 +0300

    add rm default nginx files

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo rm -rf /etc/nginx/sites-available/default        
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
