6740b0e4da6a27865a59a8c595fb87054fe5f850
vladimir <vladimir@MacBook-Pro-vladimir.local>
2016-11-29 14:52:50 +0300

    add gunicorn auto install

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-sudo rm -rf /etc/nginx/sites-available/default        
+
+#delete nginx default conf
+sudo rm -rf /etc/nginx/sites-available/default
 sudo rm -rf /etc/nginx/sites-enabled/default
+
+#create symlink on our nginx config and restart
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
+#create symlink for our gunicorn config and restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
