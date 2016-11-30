87e0fedbce7e6c641347482781a90b3163f664a2
vladimir <vladimir@MacBook-Pro-vladimir.local>
2016-12-01 00:06:35 +0300

    add ask app gunicorn config auto link

--- init.sh
+++ init.sh
@@ -9,5 +9,6 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 #create symlink for our gunicorn config and restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
