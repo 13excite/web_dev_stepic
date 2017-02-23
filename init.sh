9253ce8ca13f8c13fcb27bc02d62bd74b78f3776
Vladimir <originalblack@mail.ru>
2017-02-24 01:22:32 +0300

    change mysql init

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ sudo rm -rf /etc/nginx/sites-available/default
 sudo rm -rf /etc/nginx/sites-enabled/default
 
 #create symlink on our nginx config and restart
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/ask.conf
 sudo /etc/init.d/nginx restart
 
 #create symlink for our gunicorn config and restart
@@ -15,6 +15,9 @@ sudo /etc/init.d/gunicorn restart
 
 # MYSQL
 sudo service mysql restart
+
+# sudo mysql -uroot -e "CREATE DATABASE qa CHARACTER SET utf8 COLLATE utf8_general_ci;"
+# sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON qa.* TO 'django'@'localhost' IDENTIFIED BY '123456';"
 
 mysql -uroot -e "CREATE DATABASE qa;"
 mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY '123456';"
