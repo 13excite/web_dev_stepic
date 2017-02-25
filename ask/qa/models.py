645ffda0c79186112b2520295101e7a9ab042e16
Vladimir <originalblack@mail.ru>
2017-02-25 15:12:41 +0000

    add new implementation models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,16 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
+
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(255)
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    likes = models.ManyToManyField(User)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(
+        User, related_name="question_like", blank=True)
+
+    class Meta:
+        ordering = ('-added_at',)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('question_detail', kwargs={'pk': self.pk})
+
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
-    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+    class Meta:
+        ordering = ('added_at',)
+
+    def __unicode__(self):
+        return 'Answer by {}'.format(self.author)
