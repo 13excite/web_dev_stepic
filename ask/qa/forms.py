8d4c8b9db4813f50dd3c2a0cf35bb7a226b26dba
Vladimir <originalblack@mail.ru>
2017-03-06 23:09:36 +0300

    add AskForm

---
+++ ask/qa/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from qa.models import Question, Answer
+from django.shortcuts import get_object_or_404
+
+
+class AskForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    text = forms.CharField(widget=forms.Textarea)
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if title.strip() == '':
+            raise forms.ValidationError(u'Title is empty', code='invalid')
+        return title
+
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        if text.strip() == '':
+            raise forms.ValidationError(u'Text is empty', code='invalid')
+        return text
+
+    def save(self):
+        self.cleaned_data['author_id'] = 1
+        ask = Question(**self.cleaned_data)
+        ask.save()
+        return ask
+
