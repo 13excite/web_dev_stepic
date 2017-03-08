93bdac7ec3bf7cea1ea9c1edb9e662d9beebd57f
Vladimir <originalblack@mail.ru>
2017-03-08 20:45:57 +0300

    add view questionAsk

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+def question_ask(request):
+    if request.method == 'POST':
+        form = AskForm(request.POST)
+        if form.is_valid():
+            ask = form.save()
+            url = reverse('question_detail', args=[ask.id])
+            return HttpResponseRedirect(url)
+    else:
+        form = AskForm()
+
+    return render(request, 'ask.html', {
+        'form': form
+    })
