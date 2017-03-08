c4a4e2f80665c733e28d5a94849ac0cc9bd5ddbf
Vladimir <originalblack@mail.ru>
2017-03-08 19:05:29 +0300

    add AnswerForm

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -25,3 +25,27 @@ class AskForm(forms.Form):
         ask.save()
         return ask
 
+class AnswerForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea)
+    question = forms.IntegerField(widget=forms.HiddenInput)
+
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        if text.strip() == '':
+            raise forms.ValidationError(u'Text is empty', code='invalid')
+        return text
+
+    def clean_question(self):
+        question = self.cleaned_data['question']
+        if question == 0:
+            raise forms.ValidationError(u'Question number incorrect',
+                                        code='invalid')
+        return question
+
+    def save(self):
+        self.cleaned_data['question'] = get_object_or_404(
+            Question, pk=self.cleaned_data['question'])
+        self.cleaned_data['author_id'] = 1
+        answer = Answer(**self.cleaned_data)
+        answer.save()
+        return answer
